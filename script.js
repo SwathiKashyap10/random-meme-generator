218a894d528f6df5188fe3b87784e794760ac2c7
swathi kashyap <swathi10kashyap@gmail.com>
2025-08-10 22:58:37 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
 const generateBtn = document.querySelector(".generateBtn");
 const meme = document.querySelector(".meme img");
-const screenshotBtn = document.querySelector(".screenshotBtn");
-
-// Set crossorigin before setting src
-meme.crossOrigin = "anonymous";
 
 function fetchMeme() {
     generateBtn.innerText = "Loading a meme......"
-    // fetch("https://meme-api.com/gimme")
-    //     .then((res) => res.json())
-    //     .then((result) => {
-    //         meme.setAttribute("src", result.url);
-    //         generateBtn.innerText = "Generate New Meme";
-    //     });
     fetch("https://meme-api.com/gimme")
-    .then(res => res.json())
-    .then(result => {
-        if (result.url.includes("i.redd.it")) {
-            fetchMeme(); // Try again if blocked source
-        } else {
+        .then((res) => res.json())
+        .then((result) => {
             meme.setAttribute("src", result.url);
             generateBtn.innerText = "Generate New Meme";
-        }
-    });
-
+        });
 }
 
 generateBtn.addEventListener("click", fetchMeme);
 
-screenshotBtn.addEventListener("click", () => {
-    html2canvas(meme, { useCORS: true }).then((canvas) => {
-        const link = document.createElement("a");
-        link.download = "meme-screenshot.png";
-        link.href = canvas.toDataURL();
-        link.click();
-    });
-});
+
+
